29db6fd3db101580e8d35710f485af0a5d394898
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-05-23 16:02:50 +0300

    pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Profile from './Pages/Profile'
+import SignIn from './Pages/SignIn'
+import SignOut from './Pages/SignOut'
+import ForgotPassword from './Pages/ForgotPassword'
+import Offers from './Pages/Offers'
 
 export default function App() {
   return (
-    <div className='bg-red-500 c-blue-700'>
-      <h1 >Hallo world</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/SignIn' element={<SignIn/>}/>
+        <Route path='/SignOut' element={<SignOut/>}/>
+        <Route path='/ForgotPassword' element={<ForgotPassword/>}/>
+        <Route path='/offers' element={<Offers/>}/>
+      </Routes>
+    </>
   )
 }
